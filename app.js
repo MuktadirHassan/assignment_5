40dbe1d5b4be146b21dccde2d2a4d959696468b2
fahmida4 <afrinfahmida4@gmail.com>
2021-02-10 23:00:19 +0600

    modified

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-.then(res=>res.json())
-.then(data=>displayMeals(data));
 
-
-const displayMeals = meals =>{
-    
-    const ul = document.getElementById('meals');
-
-    for (let i = 0; i < meals.length; i++) {
-        const meal = meals[i];
-        const li = document.createElement('li');
-        li.innerText=meal.name;
-        ul.appendChild(li);
-        
-    }
-
+const searchMeal = () => {
+    searchText = document.getElementById('search-field').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchText}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMeals(data.meals))
+} 
+const displayMeals = meals => {
+    const divContainer = document.getElementById('container');
+    meals.forEach(meal => {
+        const mealDiv = document.createElement('div');
+        mealDiv.innerHTML = `<div class="col">
+        <div class="card h-100">
+          <img src="${meal.strMealThumb}" class="card-img-top" alt="#" width="110px" height="110px">
+          
+          <div class="card-body">
+          <h5 class="card-title text-center">${meal.strMeal}</h5>
+          </div>
+        </div>
+      </div>
+        `;
+        divContainer.appendChild(mealDiv);
+    })
 }
